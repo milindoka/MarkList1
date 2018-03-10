67dbbb2c8cde4e621681d6b38d62b67deb17860c
milindoka <oak445@gmail.com>
2018-03-10 21:31:31 +0530

    Imported Marklist Project From eclipse to Android Studio, tested pdf write

---
+++ MarkList/src/main/java/in/refort/MarkList/WritePDF.java
@@ -0,0 +1,72 @@
+package in.refort.MarkList;
+
+//import org.w3c.dom.Document;
+
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by student on 3/10/18.
+ */
+
+public class WritePDF
+{
+
+
+    public Boolean write(String fname, String fcontent)
+    {
+        try {
+            /* Create file path for Pdf */
+            String fpath = "/sdcard/" + fname + ".pdf";
+            File file = new File(fpath);
+            if (!file.exists())
+                file.createNewFile();
+            //
+            //To customise the text of the pdf
+            //we can use FontFamily
+            //
+            Font bfBold12 = new Font(Font.FontFamily.TIMES_ROMAN,
+                    12, Font.BOLD, new BaseColor(0, 0, 0));
+            Font bf12 = new Font(Font.FontFamily.TIMES_ROMAN,
+                    12);
+            // create an instance of itext document
+            Document document = new Document();
+            PdfWriter.getInstance(document,
+                    new FileOutputStream(file.getAbsoluteFile()));
+            document.open();
+            //using add method in document to insert a paragraph
+            document.add(new Paragraph("My First Pdf !"));
+            document.add(new Paragraph("Hello World"));
+            // close document
+            document.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } catch (DocumentException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+}
